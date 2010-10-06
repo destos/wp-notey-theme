59fbf79b080c0d613a919c062b021664771bc59a
Patrick Forringer <destos@gmail.com>
2010-10-06 08:31:06 -0500

    commenting out needless things in footer. edit later.

--- footer.php
+++ footer.php
@@ -4,8 +4,10 @@
 	<p class="wrap">
 	&copy; Patrick Forringer - This blog is proudly powered by <a href="http://wordpress.org/">WordPress</a>
 		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+		<!--
+and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
 		<?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds.
+-->
 	</p>
 	
 <?php wp_footer(); ?>
