285602bad2a94fbff63975b18d3d5a30b860c662
Patrick Forringer <destos@gmail.com>
2010-10-11 11:10:20 -0500

    adding gists to the document write filter.

--- js/theme.js
+++ js/theme.js
@@ -79,6 +79,6 @@ window.log = function(){
   var write = doc.write;
   doc.write = function(q){ 
     log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
+    if (/gist/.test(q)) write.apply(doc,arguments);  
   };
 })(document);
